68e4d01d942e3d92cb3c3c0b20fcbde017588ae1
seun <johnsonseun15@gmail.com>
2020-08-31 18:02:11 -0600

    updates

--- Narratives.py
+++ Narratives.py
@@ -12,7 +12,8 @@ from nltk import tokenize
 import re
 import ast
 import json
-import mysql.connector
+# import mysql.connector
+import multiprocessing
 from multiprocessing import Process, Pool
 from pathos.multiprocessing import freeze_support, ProcessPool
 import sys
@@ -34,21 +35,21 @@ class Narratives(SqlFuncs, Functions):
         self.num_processes = num_processes
         self.connect = connect
     
+    # main function
     def main(self):
         connection = self.get_connection(self.connect)
         with connection.cursor() as cursor:
-            # Getting narratives from narratives table
+            # Getting already populated blogpost_id from tracker_narratives table
             blogpost_ids_query = f"""
                                     SELECT JSON_KEYS(blogpost_narratives) AS blogpost_id 
                                     from tracker_narratives 
                                     where tid = {self.tid}
                                 """
             cursor.execute(blogpost_ids_query)
-            blogpost_ids = cursor.fetchall()[0]['blogpost_id']
-
-            # self.blog_ids = self.blog_ids.replace('blogsite_id in', 'b.blogsite_id in')
-            # query1 = f"""SELECT n.blogpost_id blogpost_id, n.narratives narratives, n.entity_count entity_count FROM blogposts b left join narratives n on b.blogpost_id = n.blogpost_id where {self.blog_ids} and b.blogpost_id not in {blogpost_ids.replace('[','(').replace(']',')')}"""
-            query1 = f"""
+            results = cursor.fetchall()
+            if results:
+                blogpost_ids = results[0]['blogpost_id']
+                query1 = f"""
                         SELECT a.blogpost_id, a.narratives, a.entity_count
                         FROM (
                             SELECT blogpost_id, narratives, entity_count 
@@ -57,6 +58,19 @@ class Narratives(SqlFuncs, Functions):
                             ) a
                         where a.blogpost_id not in {blogpost_ids.replace('[','(').replace(']',')')}
                     """
+            else:
+                query1 = f"""
+                            SELECT blogpost_id, narratives, entity_count 
+                            FROM narratives 
+                            where {self.blog_ids}
+                    """
+
+
+            # Getting narratives from narratives table
+
+            # self.blog_ids = self.blog_ids.replace('blogsite_id in', 'b.blogsite_id in')
+            # query1 = f"""SELECT n.blogpost_id blogpost_id, n.narratives narratives, n.entity_count entity_count FROM blogposts b left join narratives n on b.blogpost_id = n.blogpost_id where {self.blog_ids} and b.blogpost_id not in {blogpost_ids.replace('[','(').replace(']',')')}"""
+            
             cursor.execute(query1)
             records = cursor.fetchall()
             
@@ -64,45 +78,82 @@ class Narratives(SqlFuncs, Functions):
         cursor.close() 
 
         self.process_main(records)
-        self.process_entity_count(records)
+        self.process_entity_count()
 
-        # p1 = Process(target=self.process_main, args=(records, ))
-        # p2 = Process(target=self.process_entity_count, args=(records, ))
+        # p1 = Process(target=self.process_entity_count)
+        # p2 = Process(target=self.process_main, args=(records, ))
         # p1.start()
         # p2.start()
         # p1.join()
         # p2.join()
-                
-
-        
-
-
+                       
+    # process data in parallel or single process
     def process_main(self, records):
         if not self.parallel:
-            for record in tqdm(records, total=len(records), desc="Narratives"):
+            for record in tqdm(records, total=len(records), desc=f"Narratives - TID_{self.tid}"):
                 self.process_data(record)            
         else:
             print("starting multi-process")
-            with Pool(int(self.num_processes)) as p:
-                pbar = tqdm(p.imap_unordered(self.process_data, records), desc="Narratives", ascii=True,  file=sys.stdout, total=len(records))
-                for _ in pbar:
-                    pbar.update(1)
+            # with Pool(int(self.num_processes)) as p:
+            #     pbar = tqdm(p.imap_unordered(self.process_data, records), desc="Narratives", ascii=True,  file=sys.stdout, total=len(records))
+            #     for _ in pbar:
+            #         pbar.update(1)
 
-    def process_entity_count(self, records):
-        entity_counts = [ast.literal_eval(x['entity_count']) for x in records if x['entity_count']]
-        x_ = dict(functools.reduce(operator.add, map(collections.Counter, entity_counts)))
-        terms_sorted = json.dumps({k: int(v) for k, v in sorted(x_.items(), key=lambda item: item[1], reverse = True) if k != '.'})
-        data = terms_sorted, self.tid
+            process_pool = ProcessPool(self.num_processes)
+            # pbar = tqdm(process_pool.imap(self.process_data, records), desc="Narratives", ascii=True,  file=sys.stdout, total=len(records))
+            # for x in pbar:
+            #     pbar.update(1)
+
+            for record in tqdm(process_pool.uimap(self.process_data, records), desc=f"Narratives - TID_{self.tid}", ascii=True,  file=sys.stdout, total=len(records)):
+                pass
+
+            print("Finished processing!")
+            print("\nClosing pool")
+            process_pool.close()
+            print("Joining pool")
+            process_pool.join()
+            print("Clearing pool")
+            process_pool.clear()
+            print("Finished!")
+
+
+    # Getting top terms from entity_count field for selected narratives
+    def process_entity_count(self):
+        connection = self.get_connection(connect)
+        with connection.cursor() as cursor:
+            entity_query = f"""
+                        select n.term, sum(n.occurr) occurrence
+                        from narratives, 
+                        json_table(entity_count,
+                        '$[*]' columns(
+                            term varchar(128) path '$.term',
+                            occurr int(11) path '$.occurrence'
+                            )
+                        ) 
+                        as n
+                        where {self.blog_ids}
+                        group by n.term
+                        order by occurrence desc
+                    """
+            cursor.execute(entity_query)
+            entity_record = cursor.fetchall()
+
+        connection.close()
+        cursor.close()
+
+        top_entities = {}
+        for entity in entity_record:
+            term = entity['term']
+            occurrence = entity['occurrence']
+            top_entities[term] = int(occurrence)
+
+        data = json.dumps(top_entities), self.tid
         self.update_insert("update tracker_narratives set top_entities = %s where tid = %s", data, connect)
-        # connection = self.get_connection(connect)
-        # with connection.cursor() as cursor:
-            # narratives_query = f"""select blogpost_entities, blogpost_narratives, top_entities from tracker_narratives where tid = {self.tid}"""
-            # cursor.execute(narratives_query)
-            # narratives_record = cursor.fetchall()
-            # if not narratives_record:
+        print('entity_done')
 
 
 
+    # Process, insert or update narratives for tracker
     def process_data(self, record):
         connection = self.get_connection(connect)
         with connection.cursor() as cursor:
@@ -115,24 +166,13 @@ class Narratives(SqlFuncs, Functions):
             data_narratives = record['narratives']
             
             if narratives_record:
-                blogpost_entities_checked = json.loads(narratives_record[0]['blogpost_entities']) if 'blogpost_entities' in narratives_record[0] else None
+                blogpost_entities_checked = json.loads(narratives_record[0]['blogpost_entities']) if narratives_record[0]['blogpost_entities'] else {}
                 blogpost_entities_checked[str(blogpostID)] = json.loads(entity_count)
 
-                # x_ = dict(functools.reduce(operator.add, map(collections.Counter, blogpost_entities_checked.values())))
-
-                # terms_sorted = json.dumps({k: int(v) for k, v in sorted(x_.items(), key=lambda item: item[1], reverse = True) if k != '.'})  
-                # terms_sorted = json.dumps(x_)
-
-                # terms_sorted = json.dumps("{}")
-
                 blogpost_entities_checked = json.dumps(blogpost_entities_checked)
-                bp_narratives_checked = json.loads(narratives_record[0]['blogpost_narratives']) if 'blogpost_narratives' in narratives_record[0] else None     
+                bp_narratives_checked = json.loads(narratives_record[0]['blogpost_narratives']) if narratives_record[0]['blogpost_narratives'] else {}     
                 bp_narratives_checked[str(blogpostID)] = json.loads(data_narratives)
-                bp_narratives_checked = json.dumps(bp_narratives_checked)
-
-                # top_entity_checked = ast.literal_eval(narratives_record[0]['top_entities']) if 'top_entities' in narratives_record[0] else None
-                # x_ = dict(functools.reduce(operator.add, map(collections.Counter, top_entity_checked.values()))) 
-                # terms_sorted = json.dumps({k: int(v) for k, v in sorted(x_.items(), key=lambda item: item[1], reverse = True) if k != '.'})     
+                bp_narratives_checked = json.dumps(bp_narratives_checked)  
 
                 data = blogpost_entities_checked, bp_narratives_checked, self.blog_ids, self.tid
                 self.update_insert("update tracker_narratives set blogpost_entities = %s, blogpost_narratives = %s, query = %s where tid = %s", data, connect)
@@ -145,8 +185,8 @@ class Narratives(SqlFuncs, Functions):
                 temp_narratives_checked[str(blogpostID)] = ast.literal_eval(data_narratives)
                 temp_narratives_checked = json.dumps(temp_narratives_checked)
 
-                terms_sorted = json.dumps({k: int(v) for k, v in sorted(ast.literal_eval(entity_count).items(), key=lambda item: item[1], reverse = True) if k != '.'})
-                data = self.tid, temp_blogpost_entities, temp_narratives_checked, terms_sorted, self.blog_ids
+                # terms_sorted = json.dumps({k: int(v) for k, v in sorted(ast.literal_eval(entity_count).items(), key=lambda item: item[1], reverse = True) if k != '.'})
+                data = self.tid, temp_blogpost_entities, temp_narratives_checked, "{}", self.blog_ids
                 self.update_insert("insert into tracker_narratives (tid, blogpost_entities, blogpost_narratives, top_entities, query) values (%s, %s, %s, %s, %s)", data, connect)
             
         connection.close()
@@ -160,25 +200,40 @@ if __name__ == "__main__":
 
     s = SqlFuncs(connect)
     connection = s.get_connection(connect)
-    with connection.cursor() as cursor: 
-        # Getting tracker details and blog ids
-        tid = 428 
-        tracker_details = f"""SELECT query from trackers where tid = {tid}"""
-        cursor.execute(tracker_details)
-        records_tracker = cursor.fetchall()
-        blog_ids = records_tracker[0]['query']
+    tracker_blog_id = {}
 
-        parallel = False
-        num_processes = 50
+    with connection.cursor() as cursor: 
+        # Tracker ids
+        trackers = "select tid from trackers"
+        cursor.execute(trackers)
+        records = cursor.fetchall()
+
+        for x in records[2:]:
+
+        # Getting tracker details and blog ids
+        # tid = 8
+            tid = x['tid']
+            tracker_details = f"""SELECT query from trackers where tid = {tid}"""
+            cursor.execute(tracker_details)
+            records_tracker = cursor.fetchall()
+            blog_ids = records_tracker[0]['query']
+
+            tracker_blog_id[tid] = blog_ids         
     
     connection.close()
     cursor.close()
 
-    narr = Narratives(tid, blog_ids, parallel, num_processes, connect)
-    narr.main()
 
-    end = time.time()
-    runtime_mins, runtime_secs = divmod(end - start, 60)
-    runtime_mins = round(runtime_mins, 0)
-    runtime_secs = round(runtime_secs, 0)
-    print("Time to complete: {} Mins {} Secs".format(runtime_mins, runtime_secs), f'AT - {datetime.today().isoformat()}')
+    # Run for all trackers
+    for tid in tracker_blog_id:
+        parallel = True
+        num_processes = multiprocessing.cpu_count()
+        blog_ids = tracker_blog_id[tid]
+        narr = Narratives(tid, blog_ids, parallel, num_processes, connect)
+        narr.main()
+
+        end = time.time()
+        runtime_mins, runtime_secs = divmod(end - start, 60)
+        runtime_mins = round(runtime_mins, 0)
+        runtime_secs = round(runtime_secs, 0)
+        print("Time to complete: {} Mins {} Secs".format(runtime_mins, runtime_secs), f'AT - {datetime.today().isoformat()}')
